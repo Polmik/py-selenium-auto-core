7f2f85218a17c5cf7fc37b2e99558cbcd3b9de0b
Egor R <mrpolmik@hotmail.com>
2023-10-09 20:32:44 +0300

    Added localization

---
+++ core/localization/localization_manager.py
@@ -0,0 +1,33 @@
+import json
+import os
+from typing import Optional
+
+from core import ROOT_PATH_CORE
+from core.configurations.logger_configuration import LoggerConfiguration
+from core.logging.logger import Logger
+
+
+class LocalizationManager:
+    _lang_resource = "resources\\localization\\{0}.json"
+    logger = None
+
+    def __init__(self, logger_configuration: LoggerConfiguration, logger: Logger, root_path: Optional[str] = None):
+        language = logger_configuration.language
+        self.__localization_file = self.__get_localization_file(language, root_path or ROOT_PATH_CORE)
+        self.__core_localization_file = self.__get_localization_file(language, ROOT_PATH_CORE)
+        self.logger = logger
+
+    def get_localized_message(self, message_key: str, *args) -> str:
+        local_to_use = self.__localization_file if message_key in self.__localization_file else self.__core_localization_file
+        if message_key in local_to_use:
+            return local_to_use.get(message_key).format(*args)
+        self.logger.debug(f"Cannot find localized message by key '{message_key}'")
+        return message_key
+
+    def __get_localization_file(self, language: str, root_path: str) -> dict:
+        resource_name = self._lang_resource.format(language)
+        resource_path = os.path.join(root_path, resource_name)
+        if not os.path.exists(resource_path):
+            resource_path = os.path.join(ROOT_PATH_CORE, resource_name)
+        with open(resource_path, "r") as file:
+            return json.loads(file.read())
---
+++ core/localization/localized_logger.py
@@ -0,0 +1,28 @@
+from core.configurations.logger_configuration import LoggerConfiguration
+from core.localization.localization_manager import LocalizationManager
+from core.logging.logger import Logger
+
+
+class LocalizedLogger:
+
+    def __init__(
+            self,
+            localization_manager: LocalizationManager,
+            logger: Logger,
+            logger_configuration: LoggerConfiguration
+    ):
+        self.__localization_manager = localization_manager
+        self.__logger = logger
+        self.configuration = logger_configuration
+
+    def info_element_action(self, element_type: str, element_name: str, message_key: str, *args):
+        localized_message = self.__localization_manager.get_localized_message(message_key, args)
+        self.__logger.info(f"{element_type} '{element_name}' :: {localized_message}")
+
+    def info(self, message_key: str, *args):
+        localized_message = self.__localization_manager.get_localized_message(message_key, args)
+        self.__logger.info(localized_message)
+
+    def debug(self, message_key: str, *args):
+        localized_message = self.__localization_manager.get_localized_message(message_key, args)
+        self.__logger.debug(localized_message)
