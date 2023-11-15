517eb91c757e267bce04ae653311f7bbb95f19f5
Egor R <mrpolmik@hotmail.com>
2023-11-15 16:07:44 +0300

    Moved logic to disable some debug logs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from pathlib import Path
 
@@ -11,6 +12,14 @@ from py_selenium_auto_core.utilities.root_path_helper import RootPathHelper
 def setup_session(request):
     # TODO: workaround to set calling root path, because pytest runs from the root dir
     calling_root_path = RootPathHelper.current_root_path(str(Path(__file__).parent))
-    Logger.info(f"Setting calling_root_path: {calling_root_path}")
     os.environ["calling_root_path"] = calling_root_path
+    Logger.info(f"Setting calling_root_path: {calling_root_path}")
+
+    for log_name in [
+        "selenium.webdriver.remote.remote_connection",
+        "selenium.webdriver.common.selenium_manager",
+        "urllib3.connectionpool",
+    ]:
+        logger = logging.getLogger(log_name)
+        logger.disabled = True
     yield
