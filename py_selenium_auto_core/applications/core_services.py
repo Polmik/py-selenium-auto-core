d171b93176bcd193fdd61371cc3da3fce6e4f751
Egor R <mrpolmik@hotmail.com>
2023-11-16 15:49:27 +0300

    Add generics

--- py_selenium_auto_core/applications/core_services.py
+++ py_selenium_auto_core/applications/core_services.py
@@ -1,28 +1,31 @@
 import abc
-from typing import Callable
+from typing import Callable, TypeVar
 
 from py_selenium_auto_core.applications.application import Application
 from py_selenium_auto_core.applications.startup import ServiceProvider, Startup
 
+TServProv = TypeVar("TServProv", bound=ServiceProvider, covariant=True)
+TApp = TypeVar("TApp", bound=Application, covariant=True)
+
 
 class CoreServices(abc.ABC):
-    _app_container: Application = None
-    _service_provider_container: ServiceProvider = None
+    _app_container: TApp = None
+    _service_provider_container: TServProv = None
 
     @classmethod
     def _is_application_started(cls) -> bool:
         return cls._app_container is not None and cls._app_container.is_started
 
     @classmethod
-    def _set_service_provider(cls, startup: ServiceProvider):
-        cls._service_provider_container = startup
+    def _set_service_provider(cls, service_provider: TServProv):
+        cls._service_provider_container = service_provider
 
     @classmethod
     def _get_service_provider(
         cls,
-        application_provider: Callable[[ServiceProvider], Application],
-        service_provider: Callable[[], ServiceProvider] = None,
-    ) -> ServiceProvider:
+        application_provider: Callable[[TServProv], TApp],
+        service_provider: Callable[[], TServProv] = None,
+    ) -> TServProv:
         if cls._service_provider_container is None:
             if service_provider is not None:
                 cls._service_provider_container = service_provider()
@@ -33,9 +36,9 @@ class CoreServices(abc.ABC):
     @classmethod
     def _get_application(
         cls,
-        application_provider: Callable[[ServiceProvider], Application],
-        service_provider: Callable[[], ServiceProvider] = None,
-    ):
+        application_provider: Callable[[TServProv], TApp],
+        service_provider: Callable[[], TServProv] = None,
+    ) -> TApp:
         if not cls._is_application_started():
             cls._app_container = application_provider(
                 cls._get_service_provider(
